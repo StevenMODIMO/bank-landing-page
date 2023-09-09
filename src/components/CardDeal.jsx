612b5a527d1fc17248517abc71316dee1b1b6c0b
StevenMODIMO <stevepetes26@gmail.com>
2023-09-09 13:06:59 +0300

    Hoobank landing page

---
+++ src/components/CardDeal.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
+
+const CardDeal = () => (
+  <section id="" className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Find a better card deal <br className="sm:block hidden" /> in a few easy
+        steps.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Arcu tortor, purus in mattis at sad integer ffunctions. Aliquet quit
+        aliquet edget mauris tortor. c Aliquest ultrices ac, amettua
+      </p>
+      <Button styles="mt-10" />
+    </div>
+
+    <div className={layout.sectionImg}>
+      <img src={card} className="w-[100%] h-[100%]" />
+    </div>
+  </section>
+);
+
+export default CardDeal;
---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import Business from "./Business";
+import Billing from "./Billing";
+import CardDeal from "./CardDeal";
+import CTA from "./CTA";
+import Clients from "./Clients";
+import Footer from "./Footer";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+
+export {
+  Business,
+  Billing,
+  CardDeal,
+  CTA,
+  Clients,
+  Footer,
+  Stats,
+  Testimonials,
+  Navbar,
+  Hero,
+};
